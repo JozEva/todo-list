36527ba4baddb172ee81e9b9d58a8a54cb990920
Efka97 <ejozokas@gmail.com>
2022-01-25 11:48:41 +0200

    add/remove a todo item

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,58 @@
-import './App.css';
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    const id = todos.length ? todos[todos.length - 1].id + 1 : 0;
+
+    const todo = {
+      id: id,
+      value: input,
+    };
+
+    if (!input) return;
+
+    setTodos((oldList) => [...oldList, todo]);
+    setInput("");
+  };
+
+  const removeTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+
+    setTodos(updatedTodos);
+  };
+
+  console.log(todos)
   return (
-    <div className="App">
-
-    </div>
+    <Container>
+      <Typography variant="h4" component="h1">
+        title
+      </Typography>
+      <Box>
+        <TextField
+          label="Add todo..."
+          variant="standard"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button variant="contained" onClick={addTodo}>
+          Add
+        </Button>
+      </Box>
+      {todos.map((todo) => (
+        <Box key={todo.id}>
+          <p>{todo.value}</p>
+          <Button variant="contained" onClick={() => removeTodo(todo.id)}>
+            Delete
+          </Button>
+        </Box>
+      ))}
+    </Container>
   );
-}
+};
 
 export default App;
